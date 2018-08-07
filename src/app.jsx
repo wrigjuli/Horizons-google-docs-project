b9e53c7e8f83850eba29341dd35ca7a94b8f25a1
Julie Wright <wrigjuli@oregonstate.edu>
2018-08-07 10:25:32 -0700

    finished setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
+      <RaisedButton color="primary">Bold</RaisedButton>
     </div>);
   }
 }
