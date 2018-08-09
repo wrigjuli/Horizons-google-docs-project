f3d975c14d754e72d794d46a6833de494f46b067
Ahilya Mehta <ahilyamehta@gmail.com>
2018-08-09 11:19:36 -0700

    thursday third commit

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -8,17 +8,16 @@ import { MenuItem, Popover, RaisedButton } from 'material-ui';
 const socket = io('http://localhost:1337');
 /* Define custom styles */
 const customStyleMap = {
-  small: {
-    fontSize: '12px',
+  pink: {
+    color: 'pink',
   },
-  medium: {
-    fontSize: '16px',
+  red: {
+    color: 'red',
   },
-  large: {
-    fontSize: '20px',
+  black: {
+    color: 'black',
   },
 };
-
 /* Have draft-js-custom-styles build help functions for toggling font-size, color */
 const {
   styles,
@@ -46,8 +45,9 @@ export default class TextEditor extends React.Component {
     super(props);
     this.state = {
       editorState: EditorState.createEmpty(),
-      fontMenuOpen: false,
+      fontSizeOpen: false,
       anchorEl: null,
+      fontColorOpen: false,
     };
     this.onChange = editorState => this.setState({ editorState });
   }
@@ -65,7 +65,7 @@ export default class TextEditor extends React.Component {
         body: 'this is so cool',
         createdby: {
           username: 'Julie',
-          password: 'hello1'}
+          password: 'hello1' }
         },
         (res) => console.log("Saved?", res))
 
@@ -75,104 +75,124 @@ export default class TextEditor extends React.Component {
     console.log('close');
     this.setState({
       anchorEl: null,
-      fontMenuOpen: false,
+      fontSizeOpen: false,
+      fontColorOpen: false,
     });
   }
 
-  _onBoldClick(e) {
-    e.preventDefault();
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  }
-  _onUClick(e) {
-    e.preventDefault();
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-  }
-  _onIClick(e) {
-    e.preventDefault();
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-  }
-  _onCenterClick(e) {
-    console.log('center');
-    e.preventDefault();
-    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-center'));
-  }
-  _onLeftClick(e) {
-    console.log('left');
-    e.preventDefault();
-    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-left'));
-  }
-  _onRightClick(e) {
-    console.log('right');
-    e.preventDefault();
-    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-right'));
-  }
-  _onJustifyClick(e) {
-    console.log('just');
-    e.preventDefault();
-    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-justify'));
-  }
-  _onSizeClick(e) {
-    e.preventDefault();
-    // this.onChange(styles.fontSize.toggle(this.state.editorState, '45px'));
-    this.setState({
-      fontMenuOpen: true,
-      anchorEl: e.currentTarget
-    })
-  }
+  // Format Text
+    _onBoldClick(e) {
+      e.preventDefault();
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
+    _onUClick(e) {
+      e.preventDefault();
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+    }
+    _onIClick(e) {
+      e.preventDefault();
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+    }
 
-  handleSizeChange(e, size){
-    e.preventDefault();
-    this.onChange(styles.fontSize.toggle(this.state.editorState, size));
-      }
+    // Placement of Content on Page
+    _onCenterClick(e) {
+      console.log('center');
+      e.preventDefault();
+      this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-center'));
+    }
+    _onLeftClick(e) {
+      console.log('left');
+      e.preventDefault();
+      this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-left'));
+    }
+    _onRightClick(e) {
+      console.log('right');
+      e.preventDefault();
+      this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-right'));
+    }
+    _onJustifyClick(e) {
+      console.log('just');
+      e.preventDefault();
+      this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'text-align-justify'));
+    }
 
-  // _onBulletClick(e) {
-  //   e.preventDefault();
-  //   this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'bullet-lists'));
-  // }  --> make this in an image of lists
+    // Text Size Changes
+    _onSizeClick(e) {
+      e.preventDefault();
+      // this.onChange(styles.fontSize.toggle(this.state.editorState, '45px'));
+      this.setState({
+        fontSizeOpen: true,
+        anchorEl: e.currentTarget,
+      });
+    }
 
+    handleSizeChange(e, size) {
+      e.preventDefault();
+      this.onChange(styles.fontSize.toggle(this.state.editorState, size));
+    }
 
-  render() {
+   //Color Changes
+    _onColorClick(e) {
+      e.preventDefault();
+      this.setState({
+        fontColorOpen: true,
+        anchorEl: e.currentTarget,
+      });
+    }
 
-    console.log('menu open', this.state.fontMenuOpen);
-
-    return (<div>
-      <h1>Horizons Docs</h1>
-      <RaisedButton color="primary" onMouseDown={(e) => this._onBoldClick(e)}>BOLD</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onUClick(e)}>U</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onIClick(e)}>I</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onSizeClick(e)}>Size</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onCenterClick(e)}>Center</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onLeftClick(e)}>Left</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onRightClick(e)}>Right</RaisedButton>
-      <RaisedButton onMouseDown={(e) => this._onJustifyClick(e)}>Justify</RaisedButton>
-      {/* <img src="" /> */}
+    handleColorChange(e, color) {
+      e.preventDefault();
+      this.onChange(styles.color.toggle(this.state.editorState, color));
+    }
 
 
-      <Popover
-        open={this.state.fontMenuOpen}
-        onRequestClose={this.handleMenuClose.bind(this)}
-        anchorEl={this.state.anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left'
-        }}
-        >
-        <MenuItem onClick={(e) => this.handleSizeChange(e, '12px')}>Small</MenuItem>
-        <MenuItem onClick={(e) => this.handleSizeChange(e, '36pxpx')}>Medium</MenuItem>
-        <MenuItem onClick={this.handleMenuClose.bind(this)}>Large</MenuItem>
-      </Popover>
+      render() {
+        console.log('menu open', this.state.fontSizeOpen);
+
+        return (<div>
+          <h1>Horizons Docs</h1>
+          <RaisedButton color="primary" onMouseDown={(e) => this._onBoldClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133731.svg" /></RaisedButton>
+          <RaisedButton onMouseDown={(e) => this._onUClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133729.svg" /></RaisedButton><br/>
+          <RaisedButton onMouseDown={(e) => this._onIClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133730.svg" /></RaisedButton>
+          <RaisedButton onMouseDown={(e) => this._onSizeClick(e)}>Text Size</RaisedButton><br/>
+          <RaisedButton onMouseDown={(e) => this._onCenterClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133718.svg" /></RaisedButton>
+          <RaisedButton onMouseDown={(e) => this._onLeftClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133720.svg" /></RaisedButton><br/>
+          <RaisedButton onMouseDown={(e) => this._onRightClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133719.svg" /></RaisedButton>
+          <RaisedButton onMouseDown={(e) => this._onJustifyClick(e)}><img style={{width:'15px'}} src="https://image.flaticon.com/icons/svg/133/133717.svg" /></RaisedButton><br/>
+          <RaisedButton onMouseDown={(e) => this._onColorClick(e)}>Text Color</RaisedButton>
 
 
+        <Popover
+          open={this.state.fontSizeOpen}
+          onRequestClose={this.handleMenuClose.bind(this)}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          >
+          <MenuItem onClick={(e) => this.handleSizeChange(e, '12px')}>Small</MenuItem>
+          <MenuItem onClick={(e) => this.handleSizeChange(e, '36px')}>Medium</MenuItem>
+          <MenuItem onClick={(e) => this.handleSizeChange(e, '54px')}>Large</MenuItem>
+        </Popover>
 
-
-      {/* <Menu id="fontMenu"
-        open={false}
-        onClose={() => this.handleMenuClose()}
-      >
-        <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-        <MenuItem onClick={this.handleClose}>My account</MenuItem>
-        <MenuItem onClick={this.handleClose}>Logout</MenuItem>
-      </Menu> */}
+        <Popover
+          open={this.state.fontColorOpen}
+          onRequestClose={this.handleMenuClose.bind(this)}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          >
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'yellow')}>Yellow</MenuItem>
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'pink')}>Pink</MenuItem>
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'red')}>Red</MenuItem>
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'blue')}>Blue</MenuItem>
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'green')}>Green</MenuItem>
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'black')}>Black</MenuItem>
+          <MenuItem onClick={(e) => this.handleColorChange(e, 'white')}>White</MenuItem>
+        </Popover>
 
       <Editor
         editorState={this.state.editorState}
